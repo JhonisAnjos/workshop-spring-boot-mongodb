9e560dd489c2632fdc2af25617970941d053add1
Jhonis Santos dos Anjos <jhonis_10@hotmail.com.br>
2022-05-25 16:19:46 -0300

    Criando Post com User aninhado

---
+++ src/main/java/com/jhonis/workshopmongo/domain/Post.java
@@ -0,0 +1,35 @@
+package com.jhonis.workshopmongo.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.EqualsAndHashCode.Include;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Document
+public class Post implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Include
+	@Id
+	private String id;
+	
+	private Date date;
+	
+	private String title;
+	
+	private String body;
+	
+	private User author;
+}
